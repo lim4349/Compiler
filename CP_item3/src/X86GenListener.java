b50ddb67d41c31c4d6b010b10724d259e321f8ee
GOD SG <rockmanzero@naver.com>
2018-12-20 13:22:48 +0900

    어셈 시작, 마지막 부분 추가

--- CP_item3/src/X86GenListener.java
+++ CP_item3/src/X86GenListener.java
@@ -2,7 +2,17 @@
 public class X86GenListener extends MiniGoBaseListener{
 
 	@Override
-	public void exitProgram(MiniGoParser.ProgramContext ctx) {
+	public void enterProgram(MiniGoParser.ProgramContext ctx) {
+		System.out.println("push ebp");
+		System.out.println("mov ebp, esp");
+		System.out.println("push ecx");
+	}
 	
+	@Override
+	public void exitProgram(MiniGoParser.ProgramContext ctx) {
+		System.out.println();
+		System.out.println("mov esp, ebp");
+		System.out.println("pop ebp");
+		System.out.println("ret");
 	}
 }
