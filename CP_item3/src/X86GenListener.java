1e954a65d631fcf81029b8b5d1390d85d92df172
khe0613 <khe0616@cnu.ac.kr>
2018-12-22 22:16:47 +0900

    exit함수들 추가, 변수 이름 일부 변경, Variable 클래스 변경, 유용한 함수들 추가

--- CP_item3/src/X86GenListener.java
+++ CP_item3/src/X86GenListener.java
@@ -1,13 +1,18 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
+import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ParseTreeProperty;
 
 public class X86GenListener extends MiniGoBaseListener{
 	ParseTreeProperty<String> newTexts = new ParseTreeProperty<>(); // 각 ctx에 해당 되는 어셈블리 코드를 저장
 	
 	// 변수 테이블, Key -> 함수명, Value -> 그 함수에 속하는 지역 변수 리스트
-	HashMap<String, List<Variable>> local_var_table = new HashMap<>();
-	List<Variable> list = new ArrayList<Variable>();
+	HashMap<String, List<Variable>> var_table = new HashMap<>();
+	//List<Variable> list = new ArrayList<Variable>();
 	
 	@Override
 	public void enterProgram(MiniGoParser.ProgramContext ctx) {
@@ -25,12 +30,123 @@ public class X86GenListener extends MiniGoBaseListener{
 		System.out.println("\tmov esp, ebp");
 		System.out.println("\tpop ebp");
 		System.out.println("\tret");
+		
+		try {
+			make_x86_file("만들어진 어셈 코드");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void make_x86_file(String code) throws IOException {
+		File file = new File("test.asm");
+		
+		if(!file.exists()) {
+			file.createNewFile();
+		}
+		
+		BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+		writer.write(code);
+		writer.flush();
+		writer.close();
+	}
+	
+	@Override
+	public void exitDecl(MiniGoParser.DeclContext ctx) {
+	
+	}
+	
+	@Override
+	public void exitVar_decl(MiniGoParser.Var_declContext ctx) {
+	
+	}
+	
+	@Override
+	public void exitType_spec(MiniGoParser.Type_specContext ctx) {
+	
+	}
+	
+	
+	@Override
+	public void exitFun_decl(MiniGoParser.Fun_declContext ctx) {
+	
+	}
+	
+	
+	@Override
+	public void exitParams(MiniGoParser.ParamsContext ctx) {
+		
+	}
+	
+	@Override
+	public void exitParam(MiniGoParser.ParamContext ctx) {
+		
+	}
+	
+	
+	@Override
+	public void exitStmt(MiniGoParser.StmtContext ctx) {
+		
+	}
+	
+	
+	@Override
+	public void exitExpr_stmt(MiniGoParser.Expr_stmtContext ctx) {
+		
+	}
+	
+	@Override
+	public void exitAssign_stmt(MiniGoParser.Assign_stmtContext ctx) {
+		
+	}
+	
+	@Override
+	public void exitCompound_stmt(MiniGoParser.Compound_stmtContext ctx) {
+		
+	}
+	
+	@Override
+	public void exitIf_stmt(MiniGoParser.If_stmtContext ctx) {
+		
+	}
+	
+	@Override
+	public void exitFor_stmt(MiniGoParser.For_stmtContext ctx) {
+		
+	}
+	
+	@Override
+	public void exitReturn_stmt(MiniGoParser.Return_stmtContext ctx) {
+		
 	}
 	
 	@Override 
-	public void enterFor_stmt(MiniGoParser.For_stmtContext ctx) {
+	public void enterLocal_decl(MiniGoParser.Local_declContext ctx) { // var x int
+		if(ctx.getChild(2).getText().equals("int")) {
+			list.add(new Variable(ctx.getChild(1).getText(), (list.size() + 1) * 4));
+			local_var_table.put("main", list);
+		}
 		
 	}
+	
+	@Override
+	public void exitLocal_decl(MiniGoParser.Local_declContext ctx) {
+		String local_decl;
+		
+	}
+	
+	@Override
+	public void exitExpr(MiniGoParser.ExprContext ctx) {
+		
+	}
+	
+	@Override
+	public void exitArgs(MiniGoParser.ArgsContext ctx) {
+		
+	}
+	
+	
+
 	@Override
 	public void enterAssign_stmt(MiniGoParser.Assign_stmtContext ctx) { // var z int = 0
 		if(ctx.getChild(2).getText().equals("int")) {
@@ -42,25 +158,50 @@ public class X86GenListener extends MiniGoBaseListener{
 		
 		
 	}
-	@Override 
-	public void enterLocal_decl(MiniGoParser.Local_declContext ctx) { // var x int
-		if(ctx.getChild(2).getText().equals("int")) {
-			list.add(new Variable(ctx.getChild(1).getText(), (list.size() + 1) * 4));
-			local_var_table.put("main", list);
+	
+	// 매개 변수로 넘어온 context가 fun_decl의 자식일 경우, function 이름 반환
+	private String get_function_name(ParserRuleContext ctx) {
+		while(ctx.getRuleIndex() != 4) {
+			if(ctx.getRuleIndex() == 0) {
+				return null;	//매개 변수로 넘어온 context가 fun_decl의 자식이 아닌 경우
+			}
+			ctx = ctx.getParent();
+		}
+		return ctx.getChild(1).toString();	// 함수이름 반환
+	}
+	
+	
+	// 변수 테이블을 초회해서, 인자로 넘긴 함수명(또는 "global"), 변수명 에 맞는 변수를 찾는 함수
+	private Variable find_variable(String function_or_global_name, String var_name) {
+		ArrayList<Variable> variable_list = (ArrayList<Variable>) var_table.get(function_or_global_name);
+		Variable variable = null;
+		for(int i = 0; i < variable_list.size(); i++) {
+			variable = variable_list.get(i);
+			
+			if(variable.name.equals(var_name)) {
+				break;
+			}else {
+				variable = null;
+			}
 		}
 		
+		return variable;
 	}
+	
 	// 변수에 대한 클래스
 	private static class Variable{
 		String name;	// 변수명
-		int size;		// 변수 사이즈 (예 : Integer의 경우 4 바이트 이므로 4)
+		int value;		// 변수값
+		int offset;		// ebp로 부터의 변수의 offset (예 : [ebp-4]에 저장된 경우 4
+						//							[ebp-8]에 저장된 경우 8)
 		
-		public Variable(String name, int size) {
+		public Variable(String name, int value, int offset) {
 			this.name = name;
-			this.size = size;
+			this.value = value;
+			this.offset = offset;
 		}
 		public String toString() {
-			return this.name + this.size;
+			return "name : " + this.name +", value : " + this.value +", offset : " + this.offset;
 		}
 	}
 }
