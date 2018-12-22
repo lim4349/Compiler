8d513b92d07b9b2a5069d9f10f88f23353d26c77
GOD SG <rockmanzero@naver.com>
2018-12-22 20:02:38 +0900

    assignment 부분 구현

--- CP_item3/src/X86GenListener.java
+++ CP_item3/src/X86GenListener.java
@@ -1,5 +1,4 @@
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 import org.antlr.v4.runtime.tree.ParseTreeProperty;
 
@@ -8,24 +7,49 @@ public class X86GenListener extends MiniGoBaseListener{
 	
 	// 변수 테이블, Key -> 함수명, Value -> 그 함수에 속하는 지역 변수 리스트
 	HashMap<String, List<Variable>> local_var_table = new HashMap<>();
-	
+	List<Variable> list = new ArrayList<Variable>();
 	
 	@Override
 	public void enterProgram(MiniGoParser.ProgramContext ctx) {
-		System.out.println("push ebp");
-		System.out.println("mov ebp, esp");
-		System.out.println("push ecx");
+		System.out.println("global main");
+		System.out.println("section .text");
+		System.out.println("main:");
+		System.out.println("\tpush ebp");
+		System.out.println("\tmov ebp, esp");
 	}
 	
 	@Override
 	public void exitProgram(MiniGoParser.ProgramContext ctx) {
-		System.out.println();
-		System.out.println("mov esp, ebp");
-		System.out.println("pop ebp");
-		System.out.println("ret");
+		int size = local_var_table.get("main").get(0).size; //x의 size
+		//다른 변수 안쓰고 int로 고정이면 그냥 4로해도됨
+		System.out.println("\tsub esp, 0x" + local_var_table.get("main").size() * size);
+		System.out.println(); // 구분하기위해 넣어둠, 최종에는 제거
+		System.out.println("\tmov esp, ebp");
+		System.out.println("\tpop ebp");
+		System.out.println("\tret");
 	}
 	
-	
+	@Override 
+	public void enterFor_stmt(MiniGoParser.For_stmtContext ctx) {
+		
+	}
+	@Override
+	public void enterAssign_stmt(MiniGoParser.Assign_stmtContext ctx) { // var z int = 0
+		if(ctx.getChild(2).getText().equals("int")) {
+			list.add(new Variable(ctx.getChild(1).getText(), 4));
+			local_var_table.put("main", list);
+		}
+		
+		
+	}
+	@Override 
+	public void enterLocal_decl(MiniGoParser.Local_declContext ctx) { // var x int
+		if(ctx.getChild(2).getText().equals("int")) {
+			list.add(new Variable(ctx.getChild(1).getText(), 4));
+			local_var_table.put("main", list);
+		}
+		
+	}
 	// 변수에 대한 클래스
 	private static class Variable{
 		String name;	// 변수명
@@ -34,6 +58,9 @@ public class X86GenListener extends MiniGoBaseListener{
 		public Variable(String name, int size) {
 			this.name = name;
 			this.size = size;
+		}
+		public String toString() {
+			return this.name + this.size;
 		}
 	}
 }
