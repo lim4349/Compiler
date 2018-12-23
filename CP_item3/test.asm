bfcdc5ecc90131ef22a7aaf93f10436c942a3d80
khe0613 <khe0616@cnu.ac.kr>
2018-12-23 15:15:23 +0900

    완성

--- CP_item3/test.asm
+++ CP_item3/test.asm
@@ -2,15 +2,32 @@ extern printf
 global main
 
 section .data
-section.text
+	message1 db "%d", 10, 0
+section .text
 main:
 	push ebp
 	mov ebp, esp
 	sub esp, 0x8
 	mov dword [ebp-0x8], 0x0
 	mov dword [ebp-0x4], 0x19a
+L1:
+	cmp dword [ebp-0x4], 0x0
+	jle L2
+	mov eax, dword [ebp-0x4]
+	sar eax, 1
+	mov dword [ebp-0x4], eax
+	mov eax, dword [ebp-0x8]
+	mov ebx, dword [ebp-0x4]
+	add eax, ebx
+	mov dword [ebp-0x8], eax
+	jmp L1
+L2:
+	mov eax, dword [ebp-0x8]
+	push eax
+	push dword message1
+	call printf
 	mov esp, ebp
 	pop ebp
 	mov eax, 1
-	mvo ebx, 0
+	mov ebx, 0
 	int 80h
